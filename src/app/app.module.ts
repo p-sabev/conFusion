9641541104fe907946b31380bed27af752317811
pepochko <psybev@abv.bg>
2017-10-10 11:45:58 +0300

    Angular Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
@@ -22,7 +22,9 @@ import { MenuComponent } from './menu/menu.component';
     FormsModule,
     HttpModule,
     MatToolbarModule, 
-    MatButtonModule, 
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule, 
     MatCheckboxModule,
     FlexLayoutModule
   ],
