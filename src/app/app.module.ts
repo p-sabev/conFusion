79c601ec7e4aec4ed11800fc0f2b85e77184eba9
pepochko <psybev@abv.bg>
2017-10-16 16:34:16 +0300

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +32,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCheckboxModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
