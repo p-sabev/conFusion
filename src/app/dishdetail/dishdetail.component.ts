1106a4147c102b9fc9bb95e040722ef590006621
pepochko <psybev@abv.bg>
2017-10-30 13:19:02 +0200

    End-to-End Test

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,26 +9,21 @@ import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 import 'rxjs/add/operator/switchmap';
 
 @Component({
   selector: 'dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss'],
+  styleUrls: ['./dishdetail.component.scss'], 
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(), 
+    visibility(),
+    expand()
   ]
 })
 
